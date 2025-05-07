bda188b60adfd73e60ba16bf89cc6d5150a1b8b0
Aaron <acostaaaron@iresm.edu.ar>
2025-05-07 19:24:54 -0300

    usar DATABASE_URL para postgresSQL en render

--- tienda/settings.py
+++ tienda/settings.py
@@ -70,15 +70,12 @@ TEMPLATES = [
 
 # Base de datos (usa PostgreSQL si se proporciona DATABASE_URL, sino SQLite para desarrollo local)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
+
 
 
 # Validación de contraseñas
